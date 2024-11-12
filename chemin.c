36435e12e645f78b113f581ff353096b11a0bee5
Rafael Fellbom <rafael.fellbom@gmail.com>
2024-11-12 17:50:05 +0100

    test

--- chemin.c
+++ chemin.c
@@ -2,6 +2,21 @@
 // Created by rafaf on 12/11/2024.
 //
 #include <stdio.h>
-#include "chemin.h
+#include <stdlib.h>
+#include "chemin.h"
 #include "map.h"
 #include "loc.h"
+
+
+t_node* create_node(int val, int nb_sons){
+    t_node *n_node;
+    n_node = (t_node *)malloc(sizeof(t_node));
+    n_node -> nbSons = 0;
+    n_node -> value = val;
+    n_node -> sons = (t_node**)malloc(nb_sons*sizeof(t_node*));
+    for (int i=0; i < nb_sons; i++){
+        n_node -> nbSons ++;
+        n_node -> sons[i] = NULL;
+    }
+    return n_node;
+}
--- chemin.h
+++ chemin.h
@@ -5,6 +5,9 @@
 #ifndef UNTITLED1_CHEMIN_H
 #define UNTITLED1_CHEMIN_H
 
+
+
+
 typedef struct s_node
 {
     int value;
@@ -12,6 +15,13 @@ typedef struct s_node
     int nbSons;
 } t_node;
 
+typedef struct s_tree
+{
+    t_node **root ;
+} t_tree ;
+
+t_node* create_node(int val, int nb_sons);
+t_tree create_tree()
 
 #endif //UNTITLED1_CHEMIN_H
 
