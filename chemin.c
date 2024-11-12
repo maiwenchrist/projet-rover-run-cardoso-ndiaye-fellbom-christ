072920423cf0436a5ba7055285755b2b62fc3ea6
Rafael Fellbom <rafael.fellbom@gmail.com>
2024-11-12 16:59:31 +0100

    test

---
+++ chemin.c
@@ -0,0 +1,7 @@
+//
+// Created by rafaf on 12/11/2024.
+//
+#include <stdio.h>
+#include "chemin.h
+#include "map.h"
+#include "loc.h"
---
+++ chemin.h
@@ -0,0 +1,17 @@
+//
+// Created by rafaf on 12/11/2024.
+//
+
+#ifndef UNTITLED1_CHEMIN_H
+#define UNTITLED1_CHEMIN_H
+
+typedef struct s_node
+{
+    int value;
+    struct s_node **sons;
+    int nbSons;
+} t_node;
+
+
+#endif //UNTITLED1_CHEMIN_H
+
